547f1391bc14f8f81f0baaa90e50b77c28acff44
Nipaaaa1 <Frostone224@gmail.com>
2024-05-22 18:26:16 +0700

    feat(ui): add navigation for tablet

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,38 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className="flex w-full justify-between p-6">
-      <div>
-        <Image src={logo} className="size-10" alt="Space Tourism logo" />
+    <nav className="flex w-full justify-between p-6 md:p-0">
+      <div className="md:px-10 md:py-6">
+        <Image
+          src={logo}
+          className="size-10 md:size-12"
+          alt="Space Tourism logo"
+        />
       </div>
+      <ul className="navigation hidden w-full items-center justify-end gap-12 bg-white/5 pr-10 text-white  backdrop-blur-xl *:py-6 md:flex">
+        <li className="flex h-full border-b-[3px] border-white">
+          <Link className="flex gap-3 self-center" href="/">
+            <span className="font-bold">00</span>Home
+          </Link>
+        </li>
+        <li className="flex h-full">
+          <Link className="flex gap-3 self-center" href="/destination">
+            <span className="font-bold">01</span>Destination
+          </Link>
+        </li>
+        <li className="flex h-full">
+          <Link className="flex gap-3 self-center" href="/crew">
+            <span className="font-bold">02</span>Crew
+          </Link>
+        </li>
+        <li className="flex h-full">
+          <Link className="flex gap-3 self-center" href="/technology">
+            <span className="font-bold">03</span>Technology
+          </Link>
+        </li>
+      </ul>
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger className="md:hidden">
           <Image src={hamburgerMenu} className="size-max" alt="" />
         </SheetTrigger>
         <SheetContent className="border-none bg-transparent pl-8 pr-0 backdrop-blur-xl">
