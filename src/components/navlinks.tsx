926af5f45fb94982244ed4f48bf504a5a444d7a9
Nipaaaa1 <Frostone224@gmail.com>
2024-05-30 17:00:29 +0700

    revert: use next/link instead of next-view-transition

--- src/components/navlinks.tsx
+++ src/components/navlinks.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type NavLinksProps = {
