ffa64fc3d22e428f03f32bd6461cde3fc837d8ca
Nipaaaa1 <Frostone224@gmail.com>
2024-05-31 17:39:22 +0700

    refactor: extract some code from home page to components

---
+++ src/components/home/explore.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export const Explore = () => {
+  return (
+    <>
+      <div className="grid flex-1 place-items-center xl:justify-end">
+        <ExploreButton />
+      </div>
+    </>
+  );
+};
+
+const ExploreButton = () => {
+  return (
+    <div className="group relative grid size-36 place-items-center rounded-full bg-white md:size-[272px]">
+      <div className="absolute right-0 -z-[1] size-36 rounded-full bg-white/20 transition ease-in-out group-hover:scale-150 md:size-[272px]" />
+      <Link
+        href="/destination"
+        className="heading-s grid size-full place-items-center text-dark-navy"
+      >
+        EXPLORE
+      </Link>
+    </div>
+  );
+};
